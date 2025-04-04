2a9beed5965d6ff06a21a0df1108e5c709ae2b9c
Walkercito <yoanky2005@gmail.com>
2025-04-04 07:00:49 -0400

    edit: header

--- AutoReply/views/header.py
+++ AutoReply/views/header.py
@@ -1,32 +1,22 @@
 """Header view component for the AutoReply application."""
 
 import reflex as rx
-from AutoReply.styles.colors import dark_mode, gradients
-from AutoReply.styles.style import apply_text_style, spacing, container_styles
+from AutoReply.styles.colors import dark_mode, gradients, text_colors
+from AutoReply.styles.style import container_styles
 from AutoReply.components.header_text import header_text
 
 def header() -> rx.Component:
-    """
-    Renders the main header section of the application.
-
-    Returns:  
-        rx.Component: The header component  
-    """  
-    return rx.center(
-        rx.box(  
-            rx.container(  
-                rx.box(  
-                    header_text(size="h1"),  
-                    width="100%",  
-                    padding_x=["4", "4", "6", "6"],   
-                    padding_y=spacing['8'],  
-                    overflow_x="hidden",  
-                ),  
-                style=container_styles["base"],  
-                max_width="100%",  
-                overflow_x="hidden",  
-            ),  
-            width="100%",  
-            overflow_x="hidden",  
-        )
+    """Main header component."""
+    return rx.box(
+        rx.container(
+            rx.center(
+                header_text(size="h1"),
+                padding_y="12",
+                margin_top="4",
+                width="100%",
+                max_width="800px",
+            ),
+            **container_styles["base"],
+        ),
+        width="100%",
     )
