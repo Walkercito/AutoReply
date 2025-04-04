02f910bd520e275e499c094e00fb70de651468bf
Walkercito <yoanky2005@gmail.com>
2025-04-03 22:56:52 -0400

    center

--- AutoReply/views/header.py
+++ AutoReply/views/header.py
@@ -8,23 +8,25 @@ from AutoReply.components.header_text import header_text
 def header() -> rx.Component:
     """
     Renders the main header section of the application.
-    
-    Returns:
-        rx.Component: The header component
-    """
-    return rx.box(
-        rx.container(
-            rx.box(
-                header_text(size="h1"),
-                width="100%",
-                padding_x=["4", "4", "6", "6"], 
-                padding_y=spacing['8'],
-                overflow_x="hidden",
-            ),
-            style=container_styles["base"],
-            max_width="100%",
-            overflow_x="hidden",
-        ),
-        width="100%",
-        overflow_x="hidden",
+
+    Returns:  
+        rx.Component: The header component  
+    """  
+    return rx.center(
+        rx.box(  
+            rx.container(  
+                rx.box(  
+                    header_text(size="h1"),  
+                    width="100%",  
+                    padding_x=["4", "4", "6", "6"],   
+                    padding_y=spacing['8'],  
+                    overflow_x="hidden",  
+                ),  
+                style=container_styles["base"],  
+                max_width="100%",  
+                overflow_x="hidden",  
+            ),  
+            width="100%",  
+            overflow_x="hidden",  
+        )
     )
